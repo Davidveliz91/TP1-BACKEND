daddbbbe1ff4aaecefecc88604d61219767a8dd9
Davidveliz91 <davidveliz.qac@gmail.com>
2024-09-23 19:02:02 -0300

    User App structure

---
+++ index.js
@@ -0,0 +1,5 @@
+//1° recibir los argumentos pasados por la temrinal
+//2° envierselos a ./utils/createObjectUsar (en caso de crear o actualizar el usuario)
+//3° evaluar que accion quiere realizar el usuario (list, search/get, add, update, delete)
+//4° devolver el output al cliente final.
+//5°
