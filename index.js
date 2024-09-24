08c18bf1af84bb434e2fbacd504451dce71d1589
Davidveliz91 <davidveliz.qac@gmail.com>
2024-09-23 23:05:34 -0300

    Added getUsers function

--- index.js
+++ index.js
@@ -2,4 +2,3 @@
 //2° envierselos a ./utils/createObjectUsar (en caso de crear o actualizar el usuario)
 //3° evaluar que accion quiere realizar el usuario (list, search/get, add, update, delete)
 //4° devolver el output al cliente final.
-//5°
--- models.js
+++ models.js
@@ -1,24 +1,40 @@
 import { existsSync, readFileSync, writeFileSync } from "node:fs";
-import {randomUUID} from "node:crypto";
+import { randomUUID } from "node:crypto";
 //avergiuar que importar de node el hash
 //averiguar como "activar" la lectura de variables de entorno del archivo.env (dotenv)
-import {handleError} from ".utils/handleError.js";
+import { handleError } from "./utils/handleError.js";
 
 //1° recuperar variables de entorno
 //2°declarar los metodos
 
-const getUsers = () => {
-    try{
+//const userPath = path.join("./data/user.json")
 
+const PATH_FILE_USER = process.env.PATH_FILE_USER;
+const PATH_FILE_ERROR = process.env.PATH_FILE_ERROR;
+
+const getUsers = (urlfile) => {
+    try {
+        if (!urlfile) {
+            throw new Error("Access denied")
+        }
+        const exists = existsSync(PATH_FILE_USER);
+        if (!exists) {
+            writeFileSync(PATH_FILE_USER, JSON.stringify([]));
+            return [];
+        }
+        const users = JSON.parse(readFileSync(PATH_FILE_USER));
+        return users;
     } catch (error) {
-        const objError = handleError ()
-        return objError
-
+        const objError = handleError(error, PATH_FILE_ERROR);
+        return objError.type;
     }
 };
 
+/*const response = getUsers();
+console.log(response);*/
+
 const getUserById = (id) => {
-    try{
+    try {
 
     } catch (error) {
 
@@ -31,7 +47,7 @@ const getUserById = (id) => {
 //valida que el email sea un string y que no se repita
 //hashea la contraseña antes de registrar al usuario
 const addUser = (userData) => {
-    try{
+    try {
 
     } catch (error) {
 
@@ -42,7 +58,7 @@ const addUser = (userData) => {
 //si se modifica la pass deberia ser nuevamente hasheada
 //si se modifica el mail, validar que este no exista
 const updateUser = (userData) => {
-    try{
+    try {
 
     } catch (error) {
 
@@ -50,11 +66,11 @@ const updateUser = (userData) => {
 };
 
 const deleteUser = (id) => {
-    try{
+    try {
 
     } catch (error) {
 
     }
 };
 
-export {getUsers, getUserById, addUser, updateUser, deleteUser}; 
+export { getUsers, getUserById, addUser, updateUser, deleteUser }; 
